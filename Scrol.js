c55e9cafbaeeb44655c4f590d680c86aec347e4a
Vinicius.n.silva <vinicius.nascimentodasilva@outlook.com>
2023-05-07 15:26:38 -0300

    up

--- Scrol.js
+++ Scrol.js
@@ -1,3 +1,17 @@
+const menuLinks = document.querySelectorAll('.navbar');
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+// function nativeScroll(distanceFromTheTop) {
+//   window.scroll({
+//     top: distanceFromTheTop,
+//     behavior: "smooth",
+//   });
+// }
+
 function scrollToSection(event) {
   event.preventDefault();
   const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
